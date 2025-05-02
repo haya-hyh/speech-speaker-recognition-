c6974288a0c1359a537ea3b5517233bfbfc5efd9
haya <66245708+haya-hyh@users.noreply.github.com>
2025-05-02 08:33:58 +0200

    Update lab1_proto.py

--- lab1/lab1_proto.py
+++ lab1/lab1_proto.py
@@ -4,22 +4,9 @@
 import numpy as np
 from scipy.signal import lfilter,hamming
 from  scipy.fftpack import fft
-from .lab1_tools import *
+from lab1_tools import *
 
 def mspec(samples, winlen = 400, winshift = 200, preempcoeff=0.97, nfft=512, samplingrate=20000):
-    """Computes Mel Filterbank features.
-
-    Args:
-        samples: array of speech samples with shape (N,)
-        winlen: lenght of the analysis window
-        winshift: number of samples to shift the analysis window at every time step
-        preempcoeff: pre-emphasis coefficient
-        nfft: length of the Fast Fourier Transform (power of 2, >= winlen)
-        samplingrate: sampling rate of the original signal
-
-    Returns:
-        N x nfilters array with mel filterbank features (see trfbank for nfilters)
-    """
     frames = enframe(samples, winlen, winshift)
     preemph = preemp(frames, preempcoeff)
     windowed = windowing(preemph)
@@ -27,28 +14,12 @@ def mspec(samples, winlen = 400, winshift = 200, preempcoeff=0.97, nfft=512, sam
     return logMelSpectrum(spec, samplingrate)
 
 def mfcc(samples, winlen = 400, winshift = 200, preempcoeff=0.97, nfft=512, nceps=13, samplingrate=20000, liftercoeff=22):
-    """Computes Mel Frequency Cepstrum Coefficients.
-
-    Args:
-        samples: array of speech samples with shape (N,)
-        winlen: lenght of the analysis window
-        winshift: number of samples to shift the analysis window at every time step
-        preempcoeff: pre-emphasis coefficient
-        nfft: length of the Fast Fourier Transform (power of 2, >= winlen)
-        nceps: number of cepstrum coefficients to compute
-        samplingrate: sampling rate of the original signal
-        liftercoeff: liftering coefficient used to equalise scale of MFCCs
-
-    Returns:
-        N x nceps array with lifetered MFCC coefficients
-    """
-    mspecs = mspec(samples, winlen, winshift, preempcoeff, nfft, samplingrate)
+    mspecs,_ = mspec(samples, winlen, winshift, preempcoeff, nfft, samplingrate)
     ceps = cepstrum(mspecs, nceps)
     return lifter(ceps, liftercoeff)
 
 # Functions to be implemented ----------------------------------
 
-def enframe(samples, winlen, winshift, samplingrate=20000):
     """
     Slices the input samples into overlapping windows.
 
@@ -59,6 +30,7 @@ def enframe(samples, winlen, winshift, samplingrate=20000):
         numpy array [N x winlen], where N is the number of windows that fit
         in the input signal
     """
+def enframe(samples, winlen, winshift, samplingrate=20000):
     winlen_samples = int(winlen * samplingrate)
     winshift_samples = int(winshift*samplingrate)
     n_f = int((len(samples) - winlen_samples) / winshift_samples) + 1
@@ -73,18 +45,6 @@ def enframe(samples, winlen, winshift, samplingrate=20000):
 
     
 def preemp(input, p=0.97):
-    """
-    Pre-emphasis filter.
-
-    Args:
-        input: array of speech frames [N x M] where N is the number of frames and
-               M the samples per frame
-        p: preemhasis factor (defaults to the value specified in the exercise)
-
-    Output:
-        output: array of pre-emphasised speech samples
-    Note (you can use the function lfilter from scipy.signal)
-    """
     b = [1, -p]
     a = [1] 
     output = np.zeros_like(input)
@@ -94,86 +54,79 @@ def preemp(input, p=0.97):
 
 
 def windowing(input):
-    """
-    Applies hamming window to the input frames.
-
-    Args:
-        input: array of speech samples [N x M] where N is the number of frames and
-               M the samples per frame
-    Output:
-        array of windoed speech samples [N x M]
-    Note (you can use the function hamming from scipy.signal, include the sym=0 option
-    if you want to get the same results as in the example)
-    """
     _, M = input.shape
     wind = hamming(M, sym=False)
     return input * wind
 
 def powerSpectrum(input, nfft):
-    """
-    Calculates the power spectrum of the input signal, that is the square of the modulus of the FFT
-
-    Args:
-        input: array of speech samples [N x M] where N is the number of frames and
-               M the samples per frame
-        nfft: length of the FFT
-    Output:
-        array of power spectra [N x nfft]
-    Note: you can use the function fft from scipy.fftpack
-    """
     fft_input = fft(input,nfft)
     power_spec = np.abs(fft_input)**2
     return power_spec
 
-def logMelSpectrum(input, samplingrate):
-    """
-    Calculates the log output of a Mel filterbank when the input is the power spectrum
 
-    Args:
-        input: array of power spectrum coefficients [N x nfft] where N is the number of frames and
-               nfft the length of each spectrum
-        samplingrate: sampling rate of the original signal (used to calculate the filterbank shapes)
-    Output:
-        array of Mel filterbank log outputs [N x nmelfilters] where nmelfilters is the number
-        of filters in the filterbank
-    Note: use the trfbank function provided in lab1_tools.py to calculate the filterbank shapes and
-          nmelfilters
-    """
+def logMelSpectrum(input, samplingrate):
     n_frames, nfft = input.shape
     fbank = trfbank(samplingrate, nfft)
     mel_spec = np.dot(input, fbank.T)
     log_mel = np.log(mel_spec)
-    return log_mel
+    return log_mel, fbank
 
 from scipy.fftpack.realtransforms import dct
 def cepstrum(input, nceps):
-    """
-    Calulates Cepstral coefficients from mel spectrum applying Discrete Cosine Transform
+    return dct(input)[:,: nceps]#－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
 
-    Args:
-        input: array of log outputs of Mel scale filterbank [N x nmelfilters] where N is the
-               number of frames and nmelfilters the length of the filterbank
-        nceps: number of output cepstral coefficients
-    Output:
-        array of Cepstral coefficients [N x nceps]
-    Note: you can use the function dct from scipy.fftpack.realtransforms
-    """
-    cepst = dct(input, type=2, axis=1, norm='ortho')[:, :nceps]
-    return cepst
+def compute_dist(x, y, winlen, winshift):
+    x_mfcc = mfcc(x, winlen, winshift)
+    y_mfcc = mfcc(y, winlen, winshift)
+    N = x_mfcc.shape[0]
+    M = y_mfcc.shape[0] 
+    distance = np.zeros((N, M))
+    
+    for i in range(N):
+        for j in range(M):
+            distance[i, j] = np.linalg.norm(x_mfcc[i] - y_mfcc[j])
+    return distance
 
-def dtw(x, y, dist):
-    """Dynamic Time Warping.
+# attention here dist is not function
+def dtw(dist):
+    N,M = dist.shape
+    LD = dist
+    AD = np.zeros((N, M))
+    AD[0, 0] = LD[0, 0]
 
-    Args:
-        x, y: arrays of size NxD and MxD respectively, where D is the dimensionality
-              and N, M are the respective lenghts of the sequences
-        dist: distance function (can be used in the code as dist(x[i], y[j]))
+    # boundary
+    for i in range(1, N):
+        AD[i, 0] = LD[i, 0] + AD[i-1, 0]
+    for j in range(1, M):
+        AD[0, j] = LD[0, j] + AD[0, j-1]
 
-    Outputs:
-        d: global distance between the sequences (scalar) normalized to len(x)+len(y)
-        LD: local distance between frames from x and y (NxM matrix)
-        AD: accumulated distance between frames of x and y (NxM matrix)
-        path: best path thtough AD
+    for i in range(1, N):
+        for j in range(1, M):
+            AD[i, j] = LD[i, j] + min(AD[i-1, j], AD[i, j-1], AD[i-1, j-1])
+    
+    #path findingggggg
+    path = [(N-1, M-1)]
+    i, j = N-1, M-1
+    while i > 0 and j > 0:
+        direction = np.argmin([AD[i-1, j-1], AD[i-1, j], AD[i, j-1]])
+        if direction == 0:
+            i -= 1
+            j -= 1
+        elif direction == 1:
+            i -= 1
+        else:
+            j -= 1
+        path.append((i, j))
 
-    Note that you only need to define the first output for this exercise.
-    """
+    while(i>0 or j>0):
+        if i > 0:
+            i -= 1
+            path.append((i, j))
+        else:
+            j -= 1
+            path.append((i, j))
+
+    
+    d = AD[N-1, M-1] / (N + M) # may delete
+    
+    return d, AD, path
